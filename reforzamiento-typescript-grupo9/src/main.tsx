160e340cb47ce007b8847630ffb027446f95ebb9
Breider Lopez <yessidlopez2018@gmail.com>
2025-10-03 14:21:26 -0500

    Correccion main.tsx

--- reforzamiento-typescript-grupo9/src/main.tsx
+++ reforzamiento-typescript-grupo9/src/main.tsx
@@ -1,23 +1,10 @@
-import './style.css'
-import typescriptLogo from "./assets/typescript.svg";
-import viteLogo from '/vite.svg'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import EjerciciosList from "./componentes/EjerciciosList";
 
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
-
+ReactDOM.createRoot(document.getElementById("app") as HTMLElement).render(
+  <React.StrictMode>
+    <EjerciciosList />
+  </React.StrictMode>
+);
